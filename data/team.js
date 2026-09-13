c18fadbc3d3f86871394e9423bd2cb2cc5421704
Erin Kwok <erinkwok@Erins-MacBook-Pro.local>
2026-09-12 22:51:19 -0700

    Update media team member

--- data/team.js
+++ data/team.js
@@ -6,7 +6,7 @@ export const team = [
   { name: "Josh Fung", role: "Co-President,co founder" },
   { name: "Charmaine Law", role: "Co-President,co founder" },
   { name: "Jacob Ty", role: "Events Coordinator" },
-  { name: "Nate Huang", role: "Events Coordinator" },
+  { name: "Jamie Tang", role: "Media" },
   { name: "Charlotte Chan", role: "Outreach & Communications" },
   { name: "Boniface Lee", role: "Secretary" },
 ];
